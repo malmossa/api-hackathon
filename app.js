7f314a7208ade000e2316b03b0fe77b45c3eca79
Mansor Almossa <malmossa@gmail.com>
2020-08-04 16:00:42 -0700

    home page layout complete

---
+++ app.js
@@ -0,0 +1,36 @@
+
+fetch("https://restcountries.eu/rest/v2/all")
+.then( function (res) {
+  console.log(res);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let map;
+
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 8
+//   });
+// }
