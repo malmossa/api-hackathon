8789de01cc7719cf4db22cd55fa5e78545a0d2b9
Mansor Almossa <malmossa@gmail.com>
2020-08-05 09:59:48 -0700

    countries api update

--- app.js
+++ app.js
@@ -1,36 +1,51 @@
+var countriesDropMenu = document.querySelector("#countries");
+var countries;
 
-fetch("https://restcountries.eu/rest/v2/all")
-.then( function (res) {
-  console.log(res);
+countriesDropMenu.addEventListener("change", function (event) {
+
+  displayCountryInf(event.target.value)
 })
 
+fetch("https://restcountries.eu/rest/v2/all")
+  .then(function(res) {
+    // console.log(res.json());
+    return res.json();
+  })
+  .then(function(data) {
+    // console.log(data);
+    initialize(data);
+  })
+  .catch(function(err) {
+    console.log("Error: ", err)
+  })
 
 
+function initialize(countryData) {
+  countries = countryData;
+  var options = "";
+  for ( var i = 0; i < countries.length; i ++ ) {
+    options += `<option value="${countries[i].name}">${countries[i].name}</option>`
+  }
+  countriesDropMenu.innerHTML = options;
+  displayCountryInf("Afghanistan");
+}
 
+function displayCountryInf(countryName) {
+  // var countryInfo = countries.find(country => country.name === countryName);
+  var countryInfo = countries.find(function (country) {
+    if (country.name === countryName) {
+      return country;
+    }
+  })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let map;
-
-// function initMap() {
-//   map = new google.maps.Map(document.getElementById("map"), {
-//     center: { lat: -34.397, lng: 150.644 },
-//     zoom: 8
-//   });
-// }
+  document.querySelector(".capital").textContent = countryInfo.capital;
+  document.querySelector(".area-code").textContent = `+ ${countryInfo.callingCodes[0]}`;
+  document.querySelector(".population").textContent = countryInfo.population.toLocaleString();
+  document.querySelector(".nativeName").textContent = countryInfo.nativeName;
+  document.querySelector(".language").textContent = countryInfo.languages[0].name;
+  document.querySelector(".currency").textContent = countryInfo.currencies[0].name;
+  document.querySelector(".timezone").textContent = countryInfo.timezones[0];
+  document.querySelector(".continent").textContent = countryInfo.region;
+  document.querySelector(".subregion").textContent = countryInfo.subregion;
+  document.querySelector(".flag").innerHTML =`<img src="${countryInfo.flag}" alt="" height="50" width="80">`
+}
